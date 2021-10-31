e5969e961fe072dd9778734caa13a5fabb0764ef
Maaz Zaidi <maaz.zaidi2003@gmail.com>
2021-10-31 14:45:30 -0400

    Added Page Scrolling for Contact Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,12 @@ var app = {
         })
     }
 }
+
+document.querySelectorAll('a[href^="#"]').foreach(anchor => {
+            anchor.addEventListner("click", function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute("href")).scrollIntoView({
+                    behavior : "smooth"
+                });
+            });
+        });
